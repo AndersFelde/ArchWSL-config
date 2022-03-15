e4b8369834c29a7ea941b611013918876ca0a34b
AndersFelde <anderskfelde@hotmail.com>
2022-03-15 12:37:13 +0100

    fiksa farger i terminal

--- install.sh
+++ install.sh
@@ -3,37 +3,42 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-setaf 2
-echo "Write your root password"
+printUpdate()
+{
+    tput setaf 2
+    echo "$1"
+    tput sgr0
+}
+
+printUpdate "Skriv inn root password"
 passwd
 echo
 
-setaf 2
-echo "Setting up sudoers file"
+printUpdate "Setting up sudoers file"
 echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/wheel
 echo
 
-echo "Creating user"
+printUpdate "Creating user"
 read -p "Username: " username
 useradd -m -G wheel -s /bin/bash $username
 
-echo "Password for user"
+printUpdate "Password for user"
 passwd $username
 echo
 
-echo "Setting up pacman"
+printUpdate "Setting up pacman"
 pacman-key --init
 pacman-key --populate
 pacman -Syy archlinux-keyring --noconfirm
 pacman -Syyu --noconfirm
 echo
 
-echo "Installing programs"
+printUpdate "Installing programs"
 pacman -S git zsh neovim fzf bat lsd man wget neofetch --noconfirm
 echo -ne "n\n\n\n" | pacman -S --needed base-devel
 echo
 
-echo "Installing paru"
+printUpdate "Installing paru"
 sudo -i -u $username bash << EOF
 cd ~
 git clone https://aur.archlinux.org/paru.git
@@ -44,11 +49,11 @@ rm -rf ./paru
 EOF
 echo
 
-echo "Updating paru config"
+printUpdate "Updating paru config"
 sed -i '/BottomUp/s/^#//g' /etc/paru.conf
 echo
 
-echo "Setting up zsh"
+printUpdate "Setting up zsh"
 sudo -i -u $username bash << EOF
 cd ~
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | /bin/bash
@@ -63,5 +68,4 @@ git clone https://github.com/zsh-users/zsh-autosuggestions
 EOF
 chsh --shell /bin/zsh $username
 
-setaf 2
-echo "You are now done installing, follow the rest of the install guide"
+printUpdate "You are now done installing, follow the rest of the install guide"
